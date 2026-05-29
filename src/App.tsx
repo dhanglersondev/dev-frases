e477fb3a33855b2f017fa9240a1c9becbe4cdf91
dhanglersondev <dhanglerson.dev@outlook.com>
2026-05-28 22:04:10 -0300

    Esqueleto estatico do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,37 @@ function App() {
 
   return (
     <div className='container'>
+      <img
+        alt='Logo frases'
+        src={logoImg}
+        className='logo'
+      />
 
+      <h2 className='title'>Categorias</h2>
+
+      <div className='content-area'>
+
+        <section className='category-area'>
+          <button type='button' className='category-button'>
+            Motivação
+          </button>
+
+          <button type='button' className='category-button'>
+            Bem estar
+          </button>
+        </section>
+
+        <button type='button' className='button-frase'>
+          Gerar frase
+        </button>
+
+        <h2 className='texto-frase'>
+          Frase gerada aqui! Frase gerada aqui!
+          Frase gerada aqui! Frase gerada aqui!
+          Frase gerada aqui!!
+        </h2>
+
+      </div>
     </div>
   )
 }
